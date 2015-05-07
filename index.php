c9c3c7837cba495420afd7aac6f3ac25a4646757
Tristan Whitaker <tristanrileyw@gmail.com>
2015-05-07 11:18:35 -0700

    milestone 1

---
+++ index.php
@@ -0,0 +1,5 @@
+CLIENT INFO 
+CLIENT ID c73d173254d884b89d8117954f97d9ee
+CLIENT SECRET 971776cd8c4f4af7b7a6ff36f32b68b0
+WEBSITE URL http://localhost/appacademyapi/index.php
+REDIRECT URL http://localhost/appacademyapi/index.php
