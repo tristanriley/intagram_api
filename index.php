c0855515a10d8853efbf601c00c33581bad86584
Tristan Whitaker <tristanrileyw@gmail.com>
2015-05-07 11:19:16 -0700

    milestone 2

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
-CLIENT INFO 
+<?php
+//configuration for our PHP server
+set_time_limit(0);
+ini_set('default_socket_timeout', 300);
+session_start();
+
+//makes constants using define
+define('client_id', 'c73d173254d884b89d8117954f97d9ee');
+define('client_secret', '971776cd8c4f4af7b7a6ff36f32b68b0');
+define('redirectURI', 'c73d173254d884b89d8117954f97d9ee');
+define('ImageDirectory', 'pics/');
+?>
+
+
+<!--CLIENT INFO 
 CLIENT ID c73d173254d884b89d8117954f97d9ee
 CLIENT SECRET 971776cd8c4f4af7b7a6ff36f32b68b0
 WEBSITE URL http://localhost/appacademyapi/index.php
-REDIRECT URL http://localhost/appacademyapi/index.php
+REDIRECT URL http://localhost/appacademyapi/index.php-->
