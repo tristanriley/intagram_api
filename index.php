fcd6f55e136487cd29bf94bea3ed11aee110279c
Tristan Whitaker <tristanrileyw@gmail.com>
2015-05-13 11:14:34 -0700

    milestone 4

--- index.php
+++ index.php
@@ -1,32 +1,31 @@
-<?php
-//configuration for our PHP server
-set_time_limit(0);
-ini_set('default_socket_timeout', 300);
-session_start();
 
-//makes constants using define
-define('client_ID', 'c73d173254d884b89d8117954f97d9ee');
-define('client_Secret', '971776cd8c4f4af7b7a6ff36f32b68b0');
-define('redirectURI', 'c73d173254d884b89d8117954f97d9ee');
-define('ImageDirectory', 'pics/');
-
-if(isset($_Get))
+<?php 
+	//Configuration for PHP server
+	set_time_limit(0);
+	ini_set('default_socket_timeout', 300);
+	//starts session
+	session_start();
+	//Make Constants using define
+	define('client_ID', 'c73d173254d884b89d8117954f97d9ee');
+	define('client_Secret', '971776cd8c4f4af7b7a6ff36f32b68b0');
+	define('redirectURI', 'http://localhost/Instagram_api/index.php');
+	define('ImageDirectory', 'pics/');
 ?>
-
-
 
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta name="">
-	<title></title>
-</head>
-<body>
-	<!-- create a login for people to go toInstagram API -->
-	<!-- create a link to instagram using oauth/authorizing the account -->
-	<!-- After setting client_id to blank in the beginning, along with redirect_uri then you have to echo it outfrom the constants.   -->
-	<a href="https:api.instagram/oauth/authorize/?client_id=<?php echo client_ID; ?>&redirect_url=<? echo redirectURI; ?>&response_type=code">LOGIN</a>
-	<script type="text/javascript"></script>
-</body>
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="">
+		<meta name="viewport" content="width098, intitial-scale = 1">
+		<title>InstApi</title>
+		<meta rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<!-- Creating a login for people to go and give approval for our web app to access their Instagram Account
+			 After getting aprroval we are now going to have the information so that we can play with it.
+		 -->
+		<a href="https:api.instagram.com/oauth/authorize/?client_id = <?php echo clientID; ?> & redirect_uri = <?php echo redirectURI; ?> & response_type=code">LOGIN</a>
+		<script src="js/main.js"></script>
+	</body>
 </html>
