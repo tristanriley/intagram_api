dc94be8a31d58b43847e9845a20f66b6aa021f2c
Tristan Whitaker <tristanrileyw@gmail.com>
2015-05-15 08:05:40 -0700

    milestone 6 - 13

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?php 
 	//Configuration for PHP server
 	set_time_limit(0);
@@ -6,36 +5,102 @@
 	//starts session
 	session_start();
 	//Make Constants using define
-	define('client_ID', 'c73d173254d884b89d8117954f97d9ee');
-	define('client_Secret', '971776cd8c4f4af7b7a6ff36f32b68b0');
-	define('redirectURI', 'http://localhost/Instagram_api/index.php');
+	define('clientID', '1f275f40a7a0486f81287f75fd6b2e29');
+	define('clientSecret', '61ce055ea2f34d3b963e4325afbe4f70');
+	define('redirectURI', 'http://localhost/app_api/index.php');
 	define('ImageDirectory', 'pics/');
-		if (isset($_GET['code'])) {
-		$code = ($_GET['code']);
+	//connects to instagram
+	function connectToInstagram($url){
+		$ch = curl_init();
+		curl_setopt_array($ch, array(
+			CURLOPT_URL => $url,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_SSL_VERIFYPEER => false,
+			CURLOPT_SSL_VERIFYHOST => 2,
+		));
+		$result = curl_exec($ch);
+		//closes curl handler
+		curl_close($ch);
+		return $result;
+	}
+	//gets user id
+	function getUserID($userName){
+		$url = 'https://api.instagram.com/v1/users/search?q=' . $userName . '&client_id=' . clientID;
+		$instagramInfo = connectToInstagram($url);
+		$results = json_decode($instagramInfo, true);
+		return $results['data'][0]['id'];
+	}
+	//prints images onto screen
+	function printImages($userID){
+		$url = 'https://api.instagram.com/v1/users/' . $userID . '/media/recent?client_id=' . clientID . '&count=5';
+		$instagramInfo = connectToInstagram($url);
+		$results = json_decode($instagramInfo, true);
+		//parse through the info
+		foreach ($results['data'] as $items) {
+			//gives url for each picture in results
+			$image_url = $items['images']['low_resolution']['url'];
+			echo '<img src="' . $image_url . '"/><br>';
+			//saves $img_url
+			savePictures($image_url);
+		}
+	}
+	function savePictures($image_url){
+		//echo $image_url . '<br>';
+		//using php to store the emage uro in the variable $filename
+		$filename = basename($image_url);
+		//echo $filename . '<br>';
+		//makes sure that the image isnt stored in the server
+		$destination = ImageDirectory . $filename;
+		//stores image file into server
+		file_put_contents($destination, file_get_contents($image_url));
+	}
+	if (isset($_GET['code'])) {
+		$code = $_GET['code'];
 		$url = 'https://api.instagram.com/oauth/access_token';
 		$access_token_settings = array('client_id' => clientID,
 									   	'client_secret' => clientSecret,
 										'grant_type' => 'authorization_code',
 										'redirect_uri' => redirectURI, 
 										'code' => $code);
+		//cURL is a library that calls to other API's
+		//setting curl session and insert $url
+		$curl = curl_init($url);
+		curl_setopt($curl, CURLOPT_POST, true);
+		//sets the POSTFIELDS to the array
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $access_token_settings);
+		//sets equal to 1
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		//verifies that the curl is really there
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+		//stores above information
+		$result = curl_exec($curl);
+		curl_close($curl);
+		//decodes information in $result
+		$results = json_decode($result, true);
+		$userName = $results['user']['username'];
+		//echoes the decoded information 
+		$userID = getUserID($userName);
+		printImages($userID);
 	}
-
+	else{
+	}
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8">
+		<!--<meta charset="utf-8">
 		<meta name="description" content="">
-		<meta name="viewport" content="width098, intitial-scale = 1">
-		<title>InstApi</title>
-		<meta rel="stylesheet" href="css/style.css">
+		<meta name="viewport" content="width098, intitial-scale = 1">-->
+		<title>tristansRus</title>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<!--<meta rel="author" href="humans.txt"> -->
 	</head>
 	<body>
 		<!-- Creating a login for people to go and give approval for our web app to access their Instagram Account
 			 After getting aprroval we are now going to have the information so that we can play with it.
 		 -->
-		<a href="https:api.instagram.com/oauth/authorize/?client_id = <?php echo clientID; ?> & redirect_uri = <?php echo redirectURI; ?> & response_type=code">LOGIN</a>
-		<script src="js/main.js"></script>
+		<div><a href="https://api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code">Login</a></div>
+		<!-- <script src="js/main.js"></script> -->
 	</body>
 </html>
